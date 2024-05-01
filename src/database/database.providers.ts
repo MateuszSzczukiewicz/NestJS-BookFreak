28a203966f45897177c98c5616201af1e754b5a9
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-05-01 22:15:35 +0200

    Added postgres settings

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,21 +1,27 @@
 import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async () => {
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DATABASE'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+          requestCert: true,
+        },
       });
 
       return dataSource.initialize();
     },
+    inject: [ConfigService],
   },
 ];
