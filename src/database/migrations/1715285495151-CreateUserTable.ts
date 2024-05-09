11dfd234a2cc2bbb26c61e6a1c68da657c22d0ba
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-05-09 22:51:02 +0200

    Added migrations

---
+++ src/database/migrations/1715285495151-CreateUserTable.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUserTable1715285495151 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ src/database/migrations/1715287428100-CreateBookTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateBookTable1715287428100 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
