f8ae58e2a8fd4d17d0d117412a2135e2b01e34b1
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-05-15 19:02:24 +0200

    Added book migration

--- src/database/migrations/1715287428100-CreateBookTable.ts
+++ src/database/migrations/1715287428100-CreateBookTable.ts
@@ -1,11 +1,63 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
 export class CreateBookTable1715287428100 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'book',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'author',
+            type: 'varchar',
+          },
+          {
+            name: 'bookImage',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'bookShelf',
+            type: 'enum',
+            enum: ['Reading', 'Read', 'WantToRead'],
+          },
+          {
+            name: 'userId',
+            type: 'int',
+          },
+        ],
+      }),
+      true,
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+    await queryRunner.createForeignKey(
+      'book',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('book', 'userId');
+    await queryRunner.dropTable('book');
+  }
 }
